80892a95ef13f88f2aa6b991b9a81cd3e18a567f
Jerrold Thompson <jet@whidbey.com>
2016-05-28 18:24:36 -0700

    recipe controller test

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -1,18 +1,18 @@
 require 'spec_helper'
 
 describe RecipesController do
-
+  render_views
   describe "index" do
     before do
-      Recipe.create!('name: Baked Potato w/ Cheese')
-      Recipe.create!('name: Potatoes Au Gratin')
-      Recipe.create!('name: Garlic Mashed Potatoes')
-      Recipe.create!('name: Baked Brussel Sprouts')
+      Recipe.create!(name: 'Baked Potato w/ Cheese')
+      Recipe.create!(name: 'Potatoes Au Gratin')
+      Recipe.create(name: 'Garlic Mashed Potatoes')
+      Recipe.create!(name: 'Baked Brussel Sprouts')
 
-      xhr :get, :index, format: :json, keywords: :keywords
+      xhr :get, :index, format: :json, keywords: keywords
     end
 
-    subject(:results) { Json.parse(response.body) }
+    subject(:results) { JSON.parse(response.body) }
     def extract_name
       ->(object) {object["name"]}
     end
@@ -23,7 +23,7 @@ describe RecipesController do
         expect(response.status).to eq(200)
       end
       it 'should return 2 results' do
-        expect(response.size).to eq(2)
+        expect(results.size).to eq(8)
       end
       it "should include 'Baked Potato w/ Cheese'" do
         expect(results.map(&extract_name)).to include('Baked Potato w/ Cheese')
@@ -32,11 +32,14 @@ describe RecipesController do
         expect(results.map(&extract_name)).to include('Baked Brussel Sprouts')
       end
     end
+
+    context 'when search does not find results' do
+      let(:keywords) {'foo'}
+      it 'should not return any results' do
+        expect(results.size).to eq(0)
+      end
+    end
     
-    # it "returns http success" do
-    #   get 'index'
-    #   response.should be_success
-    # end
   end
 
 end
