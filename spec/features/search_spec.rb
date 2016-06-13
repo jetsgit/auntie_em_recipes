2fd30d762807f8c535084cce777da54b43e7d50b
Jerrold Thompson <jet@whidbey.com>
2016-06-13 14:01:43 -0700

    creating recipes in recipe_spec

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper.rb'
 
 feature 'Looking up recipes', js: true do
-  scenario 'finding recipes'do
+  before do
+    Recipe.create!(name: 'Baked Potato w/ Cheese')
+    Recipe.create!(name: 'Garlic Mashed Potatoes')
+    Recipe.create!(name: 'Potatoes Au Gratin')
+    Recipe.create!(name: 'Baked Brussel Sprouts')
+  end
+  scenario 'finding recipes' do
     visit '/'
-    fill_in 'keywords', with: 'baked'
+    fill_in "keywords", with: "Baked"
     click_on "Search"
-    expect(page).to have_content('Baked Potato w/ Cheese')
+    expect(page).to have_content('Baked Potato')
     expect(page).to have_content('Baked Brussel Sprouts')
   end
 end
